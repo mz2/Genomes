bc1a97adcb4b1e783f9d9a4242af7e714aff21ca
Matias Piipari <matias.piipari@gmail.com>
2012-05-26 15:26:57 +0100

    Added beginnings of Bootstrap.js styled popovers.

--- client/genomes-client.js
+++ client/genomes-client.js
@@ -34,12 +34,33 @@ $(document).bind('DOMNodeInserted', function(event)
 			},
 	        embedMargin: 260
 		});
+		Dalliance.addFeatureListener(function(ev, hit) {
+		    console.log(miniJSONify(hit));
+			var mx = ev.clientX + window.scrollX, my = ev.clientY + window.scrollY;
+			var popup = makeElement('div', '', {className: 'popover fade right in'}, {
+                position: 'absolute',
+                top: '' + my - 50 + 'px',
+                left: '' + mx - 5 + 'px',
+                display: 'block',
+                maxWidth: '400px'
+            });
+			var popover_arrow   = makeElement('div', '', {className: 'arrow'}, {});
+			var popover_inner   = makeElement('div', '', {className: 'popover-inner'}, {});
+			var popover_title   = makeElement('h3' , 'Foobar', {className: 'popover-title'}, {});
+			var popover_content = makeElement('div', 'asdfasdf', {className: 'popover-content'}, {});
+			popup.appendChild(popover_arrow);
+			popup.appendChild(popover_inner);
+			popover_inner.appendChild(popover_title);
+			popover_inner.appendChild(popover_content);
+			Dalliance.svgHolder.appendChild(popup);
+		});
 		
 		$('#popover_test').popover(
 			{animation: true, 
 			 placement: 'right',
-			 trigger: 'hover', 
+			 trigger: 'manual', 
 			 title:'Foobar',
-			 content: '<h1>foobar</h1>'})
+			 content: '<h1>foobar</h1>'});
+		$('#popover_test').popover('show');
 	}
 });
