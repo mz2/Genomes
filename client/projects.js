2a196bc2cd3ba1115708a02264afadf9a69f4a4a
Matias Piipari <matias.piipari@gmail.com>
2012-08-12 23:25:46 +0100

    Moved favourite handling to new source code file favourites.js

--- client/projects.js
+++ client/projects.js
@@ -183,31 +183,3 @@ make_okcancel_handler({
 		evt.target.blur();
 	}
 });
-
-Template.favourite_list.events = {
-  'click .favourite-item': function (event) {
-	var favourite_name = event.target.getAttribute("data-value");
-    Session.set("favourite", favourite_name);
-    console.log("Set current favourite: ", favourite_name);
-  }
-};
-
-Template.favourite_create.events = {};
-
-Template.favourite_create.events[okcancel_events('#new-favourite')] =
-make_okcancel_handler({
-	ok: function(text, evt)
-	{
-		console.log("Favourite name: " + text);
-		if (text.length > 1)
-		{
-			console.log("Inserting favourite");
-			Favourites.insert({project: Session.get('project'), name: text})
-		}
-		else
-		{
-			console.log("Empty favourite name!");
-		}
-		evt.target.blur();
-	}
-});
