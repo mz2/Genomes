6794f8b7b1599a045973ce84c2e9478d0d84b84c
Matias Piipari <matias.piipari@gmail.com>
2012-08-12 16:43:15 +0100

    Defined basic CRUD access control for the collections Projects, Favourites, Sources, SourceKeywords.

--- server/genomes-server.js
+++ server/genomes-server.js
@@ -1,3 +1,20 @@
+// Callback to determine if modification / removal is allowed
+// Adapted from:
+// https://github.com/meteor/meteor/blob/171816005fa2e263ba54d08d596e5b94dea47b0d/examples/todos/server/access_control.js
+var canModifyOwn = function(userId, objs) {
+	return _.all(objs, function(obj) {
+		return !obj.privateTo || obj.privateTo === userId;
+	});
+};
+
+var isLoggedIn = function(userId) {
+	return userId != null;
+}
+
+var allowAlways = function() {
+	return true;
+}
+
 Meteor.startup(function () {
   // code to run on server at startup
 	Projects = new Meteor.Collection("projects");
@@ -16,4 +33,32 @@ Meteor.startup(function () {
 	});
 	
 	Meteor.publish("sources", function() { console.log("Getting sources"); return Sources.find({}); });
+	
+	Projects.allow({
+		insert: allowAlways,
+		update: canModifyOwn,
+		remove: canModifyOwn,
+		fetch: ['privateTo']
+	});
+	
+	Favourites.allow({
+	    insert: isLoggedIn,
+		update: isLoggedIn,
+		remove: isLoggedIn,
+		fetch: ['privateTo']
+	});
+
+    // cannot be removed
+	Sources.allow({
+	    insert: isLoggedIn,
+		update: isLoggedIn,
+		fetch: allowAlways
+	});
+	
+	// cannot be removed
+	SourceKeywords.allow({
+	    insert: isLoggedIn,
+		update: isLoggedIn,
+		fetch: allowAlways
+	});
 });
